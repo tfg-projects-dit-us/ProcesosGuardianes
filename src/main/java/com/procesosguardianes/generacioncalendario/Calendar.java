7e32ab1f20e370e40163fd234b906af6748e0358
wbadmin <>
2022-10-29 12:38:27 +0200

    {/src/main/java/com/procesosguardianes/generacioncalendario/Calendar.java}

--- src/main/java/com/procesosguardianes/generacioncalendario/Calendar.java
+++ src/main/java/com/procesosguardianes/generacioncalendario/Calendar.java
@@ -6,12 +6,36 @@ package com.procesosguardianes.generacioncalendario;
 
 public class Calendar implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public Calendar() {
-    }
+	@org.kie.api.definition.type.Label("mes")
+	private java.lang.Integer month;
 
+	@org.kie.api.definition.type.Label(value = "año")
+	private java.lang.Integer year;
 
+	public Calendar() {
+	}
 
+	public java.lang.Integer getMonth() {
+		return this.month;
+	}
+
+	public void setMonth(java.lang.Integer month) {
+		this.month = month;
+	}
+
+	public java.lang.Integer getYear() {
+		return this.year;
+	}
+
+	public void setYear(java.lang.Integer year) {
+		this.year = year;
+	}
+
+	public Calendar(java.lang.Integer month, java.lang.Integer year) {
+		this.month = month;
+		this.year = year;
+	}
 
 }
